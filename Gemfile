5394404f83614eb49de4eec03258dd5512863efd
Kalyn Robinson <kalyn.robinson@rtslabs.com>
2017-06-21 16:06:45 -0400

    User#create complete.

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :test do
   gem 'guard',                    '2.13.0'
   gem 'guard-minitest',           '2.4.4'
   gem 'minitest-reporters',       '1.1.14'
+  gem 'rails-controller-testing'
 end
 
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/help',   to: 'pages#help'
   get '/contact', to: 'pages#contact'
   get  '/signup',  to: 'users#new'
-  
+  post '/signup',  to: 'users#create'
+
   resources :users
 end
