3a8cc7463e2218b1b6dd80cbda133bb93c91ae02
Kalyn Robinson <kalyn.robinson@rtslabs.com>
2017-06-21 15:57:15 -0400

    Add Bootswatch theme. Restructure Sass files. Add sign up page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'pages#home'
 
   get '/help',   to: 'pages#help'
   get '/contact', to: 'pages#contact'
-  
   get  '/signup',  to: 'users#new'
+  
+  resources :users
 end
